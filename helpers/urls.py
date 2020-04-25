f0b42fa869cad3e78be98442583a1ff5c2524199
Romaric <romaric@cookieshq.co.uk>
2020-04-25 13:54:08 +0100

    Add placeholder screens for task details

--- helpers/urls.py
+++ helpers/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'helpers'
 urlpatterns = [
+    path('<int:task_id>', views.detail, name="detail"),
     path('available/', views.available, name="available"),
     path('completed/', views.completed, name="completed"),
     path('', views.index, name='index'),
--- helpers/views.py
+++ helpers/views.py
@@ -61,3 +61,10 @@ def completed(request):
     'tasks': filter(completedFilter, tasks)
   }
   return render(request, 'helpers/index.html', context)
+
+def detail(request, task_id):
+  task = filter(lambda t: t['id'] == task_id, tasks)
+  context = {
+    'task': next(task)
+  }
+  return render(request, 'helpers/detail.html', context)
