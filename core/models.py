414d7f8fb9369d9afc49714088a7939ed7b6927e
Charlie <charlie.gillions@bristolisopen.com>
2020-04-25 22:09:01 +0100

    Implemented initial data load for static types

--- core/models.py
+++ core/models.py
@@ -9,24 +9,35 @@ class User(models.Model):
     email_primary = models.CharField(max_length=15, null=True, help_text="Main email for the user.")
     email_secondary = models.CharField(max_length=15, null=True, help_text="Secondary email for the user.")
     notes = models.CharField(max_length=500, null=True, help_text="Any other notes?")
+    def __str__(self):
+        return f"User {self.id}: {self.first_name} {self.last_name}"
 
-class Action(models.Model):
+class EventType(models.Model):
     name = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return f"EventType: {self.name}"
 
 class Event(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.PROTECT, help_text="Who triggered the event?")
-    action = models.ForeignKey(Action, null=True, on_delete=models.PROTECT, help_text="What was the event?")
+    event_type = models.ForeignKey(EventType, null=True, on_delete=models.PROTECT, help_text="What was the event?")
     datetime = models.DateTimeField(null=True, help_text="When was the event?")
+    def __str__(self):
+        return f"Event: {self.id}"
 
 class Relationship(models.Model):
     user_1 = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name="user_1")
     user_2 = models.ForeignKey(User, null=True, on_delete=models.PROTECT, related_name="user_2")
     created_datetime = models.DateTimeField(null=True, help_text="When did they first make contact?")
+    def __str__(self):
+        return f"Relationship: {self.id} (between {self.user_1} and {self.user_2}"
 
 class Ward(models.Model):
     name = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return f"Ward: {self.name}"
 
 class Requester(User):
+    user_type = models.CharField(max_length=20, null=True, default="requester")
     address_line_1 = models.CharField(max_length=100, null=True, help_text="First line of their address.")
     address_line_2 = models.CharField(max_length=100, null=True, help_text="Second line of their address.")
     address_line_3 = models.CharField(max_length=100, null=True, help_text="Third line of their address.")
@@ -37,8 +48,11 @@ class Requester(User):
     confident_online_shopping = models.BooleanField(null=True, help_text="Is this person confident online shopping?")
     confident_online_comms = models.BooleanField(null=True, help_text="Is this person confident communicating online?")
     shielded = models.BooleanField(null=True, help_text="Is this person sheilded?")
+    def __str__(self):
+        return f"Requester {self.id}: {self.first_name} {self.last_name}"
 
 class Helper(User):
+    user_type = models.CharField(max_length=20, null=True, default="helper")
     dbs_number = models.CharField(max_length=12, null=True, help_text="The user's DBS certificate number, if they have one.")
     access_to_car = models.BooleanField(null=True, help_text="Does this person have access to a car?")
     driving_license = models.BooleanField(null=True, help_text="Does this person have a driving license?")
@@ -68,23 +82,35 @@ class Helper(User):
     available_sun_morning = models.BooleanField(null=True)
     available_sun_afternoon = models.BooleanField(null=True)
     available_sun_evening = models.BooleanField(null=True)
+    def __str__(self):
+        return f"Helper {self.id}: {self.first_name} {self.last_name}"
 
 class HelperWard(models.Model):
     ward = models.ForeignKey(Ward, on_delete=models.PROTECT, null=True, help_text="A ward that the user can help in.")
     helper = models.ForeignKey(Helper, on_delete=models.PROTECT, null=True, help_text="The associated user.")
+    def __str__(self):
+        return f"{self.helper.first_name} {self.helper.last_name} can help in {self.ward.name}"
 
 class HelpType(models.Model):
     name = models.CharField(max_length=50, null=True)
+    def __str__(self):
+        return f"HelpType: {self.name}"
 
 class HelpPreference(models.Model):
-    type = models.ForeignKey(HelpType, on_delete=models.PROTECT, null=True, help_text="The type of help they're happy to do.")
+    help_type = models.ForeignKey(HelpType, on_delete=models.PROTECT, null=True, help_text="The type of help they're happy to do.")
     helper = models.ForeignKey(Helper, on_delete=models.PROTECT, null=True, help_text="The associated user.")
+    def __str__(self):
+        return f"HelpPreference: {self.id}"
 
 class JobPriority(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return f"JobPriority: {self.name}"
 
 class JobStatus(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return f"JobStatus: {self.name}"
 
 class Job(models.Model):
     added_by = models.CharField(max_length=100, null=True, help_text="What's your name?")
@@ -99,3 +125,5 @@ class Job(models.Model):
     timeTaken = models.DurationField(null=True, help_text="How long did it take to help?")
     notes = models.CharField(max_length=500, null=True, help_text="Any other notes?")
     public_description = models.CharField(max_length=500, null=True, help_text="Text that gets displayed to helpers.")
+    def __str__(self):
+        return f"Job: {self.id}"
