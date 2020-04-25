d7de46d36ffdf5684a6a7dce4b38af7be2428e28
Charlie <charlie.gillions@bristolisopen.com>
2020-04-25 20:40:51 +0100

    Added models. Integrated with admin site.

--- tofro/settings.py
+++ tofro/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = DJANGO_ALLOWED_HOSTS
 
 # Application definition
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
