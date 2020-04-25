52cb10140ce49688f19bc45a786928f8e907c7d9
Charlie <charlie.gillions@bristolisopen.com>
2020-04-25 17:13:03 +0100

    WIP. Created model classes, need to test.

--- tofro/urls.py
+++ tofro/urls.py
@@ -3,10 +3,12 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('', include('polls.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
