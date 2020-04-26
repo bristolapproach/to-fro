c7fc4f793ab5159f07e49526f5a357a81e3e0f8b
Romaric <romaric@cookieshq.co.uk>
2020-04-26 16:07:31 +0100

    Handle helpers volunteering for tasks

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ import datetime
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from core.models import Job, Helper
+from core.models import Job, Helper, JobStatus
 
 
 def index(request):
@@ -19,7 +19,8 @@ def index(request):
 
 
 def available(request):
-    jobs = Job.objects.filter(helper__isnull=True)
+    jobs = Job.objects.filter(
+        helper__isnull=True, job_status__name='pending_help')
     context = {
         'currentListType': 'available',
         'title': 'Available jobs',
@@ -54,7 +55,15 @@ def detail(request, task_id):
     }
 
     if request.method == "POST":
-        messages.success(request, 'Thanks for volunteering!')
+        if (job.job_status.name != 'pending_help'):
+            messages.error(
+                request, 'Thanks, but someone has already volunteered to help')
+        else:
+            job.helper = helper
+            job.job_status = JobStatus.objects.get(name='helper_interest')
+            job.save()
+            messages.success(request, 'Thanks for volunteering!')
+        return redirect('tasks:detail', task_id=job.id)
 
     return render(request, 'tasks/detail.html', context)
 
@@ -70,6 +79,6 @@ def complete(request, task_id):
 
     if request.method == "POST":
         messages.success(request, 'Success')
-        return redirect('tasks:detail', task_id=task_id)
+        return redirect('tasks:detail', task_id=job.id)
 
     return render(request, 'tasks/complete.html', context)
