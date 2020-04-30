5f8d381ae162dc8316ec096d55a729a3cffd28d0
Romaric <hello@romaricpascal.is>
2020-04-30 16:59:01 +0100

    Add pre-filling of job descriptions based on templates

---
+++ assets/src/admin.js
@@ -0,0 +1,48 @@
+// Attribute for marking a field as edited
+const ATTR_EDITED = 'data-edited';
+
+(function () {
+  // Wait for everything to be loaded, especially jQuery
+  document.addEventListener('DOMContentLoaded', () => {
+    const $ = window.django.jQuery;
+    const data = getData(); // Read the data passed by the backend
+
+    if (data.job_description_templates) {
+      // Wait that all Django related setup code has run
+      // so we don't respond to synthetic change events triggered
+      // during their initialization
+      $(function () {
+        $(document).on('change', '[name="help_type"]', function ({ target }) {
+          const templates = data.job_description_templates[target.value] || {};
+
+          ['public', 'private'].forEach((descriptionType) => {
+            const $field = $(`[name="${descriptionType}_description"]`);
+            const descriptionTemplate = templates[`${descriptionType}_description_template`]
+            const currentDescription = ($field.val() || "").trim();
+            if (!currentDescription || !$field[0].hasAttribute('data-edited')) {
+              $field.val(descriptionTemplate)
+              $field.removeAttr('data-edited');
+            }
+
+          })
+
+          currentHelpType = target.value;
+        })
+
+        // Track editions of descriptions to prevent replacement of edited value
+        $(document).on('input', '[name="public_description"],[name="private_description"]', function ({ target }) {
+          target.setAttribute('data-edited', '');
+        });
+      });
+    }
+  });
+})();
+
+
+function getData() {
+  const element = document.getElementById('js-data');
+  if (element) {
+    return JSON.parse(element.innerText);
+  }
+  return {};
+}
--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core import serializers
 
 from core.models import EventType, Event, Relationship, Ward, Requester, Helper, \
     HelpType, JobPriority, JobStatus, Job, Notification
@@ -82,6 +83,41 @@ class JobAdmin(admin.ModelAdmin):
             'fields': ('added_by', 'call_datetime', 'call_duration')
         })
     )
+
+    def add_view(self, request, form_url='', extra_context=None):
+        """
+        Custom add view that has the list of job description by help type
+        added to the context, ready to be rendered with json_script
+        """
+        extra_context = extra_context or {}
+        extra_context['js_data'] = {
+            'job_description_templates': self.get_description_templates()
+        }
+        return super().add_view(
+            request, form_url, extra_context=extra_context,
+        )
+
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        """
+        Custom add view that has the list of job description by help type
+        added to the context, ready to be rendered with json_script
+        """
+        extra_context = extra_context or {}
+        extra_context['js_data'] = {
+            'job_description_templates': self.get_description_templates()
+        }
+        return super().change_view(
+            request, object_id, form_url, extra_context=extra_context,
+        )
+
+    def get_description_templates(self):
+        """
+        Queries and prepares an index of each HelpType description templates
+        """
+        return dict((help_type.pk, {
+            'private_description_template': help_type.private_description_template,
+            'public_description_template': help_type.public_description_template
+        }) for help_type in HelpType.objects.all())
 
 
 admin.site.register(Job, JobAdmin)
