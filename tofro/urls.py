6b62efbb1ef09e99ad927b7cec14b30024bab2ce
Romaric <hello@romaricpascal.is>
2020-05-04 10:55:22 +0100

    Add authentication wall in front of the tasks app

--- tofro/urls.py
+++ tofro/urls.py
@@ -5,13 +5,16 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.conf import settings
 from django.contrib import admin
+
+from decorator_include import decorator_include
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf.urls import url
 from core import views
+from tofro.lib import login_required
 
 urlpatterns = [
-    path('tasks/', include('tasks.urls')),
+    path('tasks/', decorator_include(login_required, ('tasks.urls', 'tasks'))),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', views.homepage, name="home")
