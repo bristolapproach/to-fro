1d2e78edb585ae41b06fe95f54082ec4b8edc55a
Romaric <hello@romaricpascal.is>
2020-05-07 16:50:40 +0100

    Add configurable help type icons

--- src/categories/models.py
+++ src/categories/models.py
@@ -10,6 +10,8 @@ class Ward(models.Model):
 
 class HelpType(models.Model):
     name = models.CharField(max_length=50, null=True)
+    icon_name = models.CharField(
+        max_length=50, null=True, help_text="Name of the fontawesome icon used to represent this type of help (among the 'free' ones: https://fontawesome.com/icons?d=gallery&m=free)")
     private_description_template = models.TextField(
         null=True, blank=True, help_text="Private description will be pre-filled with this text when picking this type of help for a Action")
     public_description_template = models.TextField(
