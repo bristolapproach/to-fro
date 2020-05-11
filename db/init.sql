7df94ea5acbe639d399d0fde80c33c09bd7e9616
Charlie <charlie.gillions@bristolisopen.com>
2020-05-11 09:59:02 +0100

    Created tofro & bcc databases & users.

--- db/init.sql
+++ db/init.sql
@@ -1,14 +1,10 @@
 -- Read variables from the environment.
-\set main_user `echo "$POSTGRES_USER"`
-
-
--- Create the database for tofro.
--- CREATE USER tofro;
--- CREATE DATABASE tofro;
--- GRANT ALL PRIVILEGES ON DATABASE tofro TO tofro;
+\set main_user `echo "${POSTGRES_USER}"`
+\set bcc_user `echo "${POSTGRES_BCC_USER}"`
+\set bcc_password `echo "${POSTGRES_BCC_PASSWORD}"`
 
 -- Create the database for BCC.
-CREATE USER bcc;
 CREATE DATABASE bcc;
-GRANT ALL PRIVILEGES ON DATABASE bcc TO bcc;
+CREATE USER :bcc_user WITH PASSWORD :'bcc_password';
+GRANT ALL PRIVILEGES ON DATABASE bcc TO :bcc_user;
 GRANT ALL PRIVILEGES ON DATABASE bcc TO :main_user;
