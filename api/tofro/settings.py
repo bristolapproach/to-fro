613d53d43dc5eb4c628cf4c3287ff3292e922138
Romaric <hello@romaricpascal.is>
2020-05-11 16:22:51 +0100

    Add links to switch between co-ordinator and volunteer view

--- api/tofro/settings.py
+++ api/tofro/settings.py
@@ -172,7 +172,7 @@ AUTHENTICATION_BACKENDS = ['core.backends.EmailBackend']
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-uk'
 
 TIME_ZONE = 'UTC'
 
