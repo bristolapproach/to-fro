dce5da0562f2abc309d914d9c2ee18a389a99c66
Romaric Pascal <hello@romaricpascal.is>
2020-05-18 12:14:09 +0100

    Move code for customization of flatpage admin into pages_and_menus

--- api/pages_and_menus/admin.py
+++ api/pages_and_menus/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
 from sitetree import admin as sitetree_admin, models
+from django.urls import reverse
+from django.contrib.sites.models import Site
+from django.contrib.flatpages import admin as flatpages_admin, models
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -36,3 +40,36 @@ class TreeItemAdmin(sitetree_admin.TreeItemAdmin):
 
 sitetree_admin.override_tree_admin(TreeAdmin)
 sitetree_admin.override_item_admin(TreeItemAdmin)
+
+
+class FlatPageAdmin(flatpages_admin.FlatPageAdmin):
+    """
+    Customization of the FlatPageAdmin
+    """
+    # Reduce the visible fields in the admin
+    fieldsets = (
+        (None, {'fields': ('url', 'title', 'content', 'registration_required')}),)
+
+    def save_model(self, request, obj, form, change):
+        logger.debug('Change ? %s', change)
+        if change:
+            # Only saved objects can get items assigned to their
+            # Many to many relationships
+            obj.sites.set(Site.objects.all())
+
+        super().save_model(request, obj, form, change)
+
+        if not change:
+            # Save the related field
+            obj.sites.set(Site.objects.all())
+            obj.save()
+
+    def view_on_site(self, obj):
+        # Fix the view on site for this object
+        # As we're on the same site, we can just
+        # reverse the URL, making sure we remove the initial slash though
+        return reverse('page', kwargs={'url': obj.url[1:]})
+
+
+admin.site.unregister(models.FlatPage)
+admin.site.register(models.FlatPage, FlatPageAdmin)
