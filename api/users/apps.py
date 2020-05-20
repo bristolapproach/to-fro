6c3411e63977758faf6057aa683c1ce254b70aee
Romaric Pascal <hello@romaricpascal.is>
2020-05-20 15:09:45 +0100

    Automatically opt-in all users to new help types and wards

--- api/users/apps.py
+++ api/users/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     name = 'users'
     verbose_name = "People"
+
+    # Import the signals when Django is ready.
+    def ready(self):
+        import users.signals
--- api/users/signals.py
+++ api/users/signals.py
@@ -1,2 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from categories.models import HelpType, Ward
+from users.models import Volunteer
+
+
+@receiver(post_save, sender=HelpType, dispatch_uid="HelpTypeOptIn")
+@receiver(post_save, sender=Ward, dispatch_uid="WardOptIn")
+def post_save_category(sender, instance, created, **kwargs):
+    """
+    Automatically opt-in all volunteers to new Wards and HelpTypes
+    so they see the new actions in their listing.
+    """
+    if created:
+        instance.volunteers.set(Volunteer.objects.all())
+        instance.save()
