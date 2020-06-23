fdcd0d49ea8bba94d99b70ea24f9b4f09eabecd6
Charlie <charlie.gillions@bristolisopen.com>
2020-06-23 22:36:10 +0100

    Volunteer not assigned at this point. Simplified complex  object creation.

--- api/notifications/utils.py
+++ api/notifications/utils.py
@@ -2,16 +2,15 @@ from django.template.loader import render_to_string
 from django.urls import reverse
 
 
-def gen_subject_and_message(site_url, notification_type, action=None, context=None):
+def gen_subject_and_message(site_url, notification_type, action=None, context={}):
     """Renders the subject and message for given notification type.
     If an action is provided, the `action_url` and `admin_action_url` will
     also be provided as context to the message template.
     """
-    # Generate message context.
-    message_context = {} if not action else context or {}
-    message_context['action_url'] = f'{site_url}{reverse("actions:detail", kwargs={"action_id":action.id})}'
-    message_context['admin_action_url'] = f'{site_url}/admin/actions/action/{action.id}/change'
+    # Generate URLs for the templates.
+    context['action_url'] = f'{site_url}{reverse("actions:detail", kwargs={"action_id":action.id})}'
+    context['admin_action_url'] = f'{site_url}/admin/actions/action/{action.id}/change'
 
     # Return the subject and message, removing line breaks from the subject.
     return (render_to_string(f'notifications/{notification_type.lower()}_subject.txt').replace('\n', ' ').replace('\r', ' '),
-            render_to_string(f'notifications/{notification_type.lower()}_message.txt', message_context))
+            render_to_string(f'notifications/{notification_type.lower()}_message.txt', context))
