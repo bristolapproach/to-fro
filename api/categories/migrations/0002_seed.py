a285cbec7363b0d89f98126de0212b0b243256e7
Charlie <charlie.gillions@bristolisopen.com>
2020-06-30 12:51:18 +0100

    Consolidated model migrations & seeding.

--- api/categories/migrations/0002_seed.py
+++ api/categories/migrations/0002_seed.py
@@ -13,7 +13,10 @@ def import_help_types(apps, schema_editor):
 
     # Create the objects.
     for help_type in data:
-        HelpType.objects.create(name=help_type.get('name')).save()
+        HelpType.objects.create(
+            name=help_type.get('name'),
+            icon_name=help_type.get('icon_name')
+        ).save()
 
 
 def import_wards(apps, schema_editor):
@@ -24,6 +27,23 @@ def import_wards(apps, schema_editor):
         Ward.objects.create(name=ward).save()
 
 
+def import_requirements(apps, schema_editor):
+    Requirement = apps.get_model('categories', 'Requirement')
+    with open("categories/initial_data/requirements.json") as data_file:
+        data = json.load(data_file)
+    for requirement in data:
+        Requirement.objects.create(name=requirement).save()
+
+
+def import_helptype_requirements(apps, schema_editor):
+    Requirement = apps.get_model('categories', 'Requirement')
+    HelpType = apps.get_model('categories', 'HelpType')
+    enhanced_dbs_requirement = Requirement.objects.get_or_create(
+        name='Enhanced DBS Check')
+    enhanced_dbs_requirement[0].help_types.set(HelpType.objects.all())
+    enhanced_dbs_requirement[0].save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -32,5 +52,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunPython(import_help_types),
-        migrations.RunPython(import_wards)
+        migrations.RunPython(import_wards),
+        migrations.RunPython(import_requirements),
+        migrations.RunPython(import_helptype_requirements)
     ]
