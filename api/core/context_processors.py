36fd8228e836934da3ea1c2f5dddf4114c0611f6
Romaric Pascal <hello@romaricpascal.is>
2020-07-21 12:25:52 +0100

    Add kite background on specific pages

--- api/core/context_processors.py
+++ api/core/context_processors.py
@@ -1,6 +1,7 @@
 import os
 import logging
 from django.conf import settings as django_settings
+from django.shortcuts import resolve_url
 
 logger = logging.getLogger(__name__)
 
@@ -11,4 +12,22 @@ def settings(request):
     """
     return {
         'settings': django_settings
+    }
+
+
+NAMES_OF_URLS_WITH_KITE = (
+    'logout',
+    'login',
+    'password_reset'
+)
+
+
+def show_kites(request):
+    """
+    Checks if the view should display a kite in the background or not
+    """
+    matches_urls_with_kites = request.resolver_match.url_name in NAMES_OF_URLS_WITH_KITE
+    is_logged_out_homepage = request.resolver_match.url_name == 'home' and not request.user.is_authenticated
+    return {
+        'show_kites': matches_urls_with_kites or is_logged_out_homepage
     }
