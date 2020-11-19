3899de0a152697fc0b2283e4b8998cec295d249a
ross <ross.mailinglists2@gmail.com>
2020-11-19 16:19:59 +0000

    removed env variable

--- api/build_scripts/build-frontend-assets.sh
+++ api/build_scripts/build-frontend-assets.sh
@@ -5,10 +5,7 @@ if [[ ! ($RUN_ENV == "local-dev" || $RUN_ENV == "dev" || $RUN_ENV == "prod") ]];
   echo "error: RUN_ENV must be one of: 'local-dev', 'dev', 'prod'"; exit 1
 fi
 
-if [[ -z $TOFRO_API_BASE_DIR ]]; then
-  echo "error: the TOFRO_API_BASE_DIR environment variable must be set"; exit 1
-fi
-
+TOFRO_API_BASE_DIR=/code
 
 if [[ $RUN_ENV == "local-dev" || $RUN_ENV == "dev" ]]; then
   NODE_ENV=development
