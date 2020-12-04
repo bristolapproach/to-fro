43775817b286df81d02d4c51bd0e6965b4bc89a6
ross <ross.mailinglists2@gmail.com>
2020-12-04 14:53:46 +0000

    email cleanup; updated frontend build docs

--- README.md
+++ README.md
@@ -35,15 +35,11 @@ You can see the names of the running containers with `docker ps`.
 
 HTML is generated using [Django views](https://docs.djangoproject.com/en/3.1/topics/http/views/). Most modules of the application will have a `views.py` with the Python code grabbing the data and a `templates` folder containing the template files to be rendered by the views when answering the requests. Some also have a `templatetags` folder defining some [new filters and tags for Django's template language](https://docs.djangoproject.com/en/3.1/howto/custom-template-tags/).
 
-The styles and scripts of the app are build with [ParcelJS](https://parceljs.org/) from the sources in `api/assets/src`. The files are compiled to the `api/assets/static` folder to match [Django's conventions for static folders](https://docs.djangoproject.com/en/3.0/howto/static-files/#configuring-static-files). This allows the files to be gathered by Django during the app startup when `python manage.py collectstatic` is run.
+The styles and scripts of the app are build with [ParcelJS](https://parceljs.org/) from the sources in `api/static-src`. The files are compiled to the `api/parcel-built`, which is in Django's `STATICFILES_DIRS` search path. These are then collected, along with images, by Django's `collectstatic` command.
 
-The build happens at container startup so you shouldn't have anything to do if you're just editing backend code. For development, you can run `npm run dev` inside the Django container to monitor your files and rebuild on change:
+These two steps (Parcel's build and Django's collectstatic) can be run together from the main frontend build script `api/build-scripts/build-frontend-assets.sh`. You will typically be running this inside the tofro-django container like so:
 
-    docker exec -it tofro-django npm run dev
-
-Equally, you can run `npm install` to install new modules from inside the Django container too:
-
-    docker exec -it tofro-django npm install
+    docker exec tofro-django /code/build_scripts/build-frontend-assets.sh
 
 ## Application messages
 
--- api/notifications/management/commands/send_digest_emails.py
+++ api/notifications/management/commands/send_digest_emails.py
@@ -1,25 +1,27 @@
 from collections import namedtuple
 import datetime
-from email.mime.image import MIMEImage
+import logging
+import os
+from smtplib import SMTPException
+import time
 
 from django.core.mail import EmailMessage
 from django.core.management.base import BaseCommand
 from django.template.loader import render_to_string
 
-from notifications.notifications import send_email
 from notifications.views import get_daily_action_sections
 from users.models import Volunteer
 
 
-import logging
 logger = logging.getLogger(__name__)
 
+RUN_ENV = os.environ.get('RUN_ENV', 'dev')
+HOSTNAMES = {  # store this in settings.py?
+    'local-dev': 'localhost:8000',
+    'dev': 'dev.tofro.hostedby.bristolisopen.com',
+    'prod': 'tofro.hostedby.bristolisopen.com'
+}
 FakeRequest = namedtuple('FakeRequest', ['scheme', 'META'])
-
-# skip when "no new available today & no available high priority & no upcoming today"
-# new_available_actions.count() == 0  (both)
-# hp_available_actions.count() == 0  (both)
-# upcoming_actions_today.count() == 0  (daily)
 
 
 class Command(BaseCommand):
@@ -78,6 +80,7 @@ class Command(BaseCommand):
                 volunteer, action_sections, today, tomorrow,
                 'Your daily digest', 'notifications/action_digest_email.html'
             )
+            time.sleep(2)  # rate-limit to avoid overwhelming email server
 
     def send_weekly_emails(self, volunteers):
         today = datetime.date.today()
@@ -100,12 +103,12 @@ class Command(BaseCommand):
                 volunteer, action_sections, today, tomorrow,
                 'Your weekly digest', 'notifications/weekly_digest_email.html'
             )
+            time.sleep(2)  # rate-limit to avoid overwhelming email server
 
     @staticmethod
     def send_digest_email(
         volunteer, action_sections, today, tomorrow, subject_title, template_file
     ):
-
         context = {
             'volunteer': volunteer,
             'action_sections': action_sections,
@@ -113,93 +116,19 @@ class Command(BaseCommand):
             'tomorrow': tomorrow,
             'title': 'Volunteer Daily Digest',
             'request': FakeRequest(
-                'https', {'HTTP_HOST': 'dev.tofro.hostedby.bristolisopen.com'}
+                'https', {'HTTP_HOST': HOSTNAMES[RUN_ENV]}
             ),
             'is_email': True
         }
-
-        # end_mail(subject, message, from_email, recipient_list, fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=None)
-
-        '''
-        import base64
-        images = [
-            ('tofro_kites', '/code/static-built/img/tofro-kites.png'),
-            ('tofro_logo_knockout', '/code/static-built/img/svg/TO_FRO_logo-04-knockout.png')
-        ]
-        images_encoded = {}
-        for slug, filepath in images:
-            with open(filepath, 'rb') as f:
-                images_encoded[slug] = base64.b64encode(f.read()).decode()
-        context.update(images_encoded)
-
-        html_body = """
-        <html>
-          <body>
-            <h3>before</h3>
-            <p>
-                <img alt="logo" src="cid:%s">
-            </p>
-            <h3>after</h3>
-          </body>
-        </html>
-        """ % cid
-
-        '''
-
-        print(f"sending email to Volunteer {volunteer.pk}: {volunteer.email}")
         html_body = render_to_string(template_file, context)
         email_msg = EmailMessage(
             subject_title, html_body,
             bcc=[volunteer.email],
         )
         email_msg.content_subtype = "html"
-        email_msg.send()
-        return
 
-
-        from django.core.mail import EmailMultiAlternatives
-        from anymail.message import attach_inline_image_file
-
-        message = EmailMultiAlternatives(
-            subject_title, 'text alternative',
-            'dev_notifications@kwmc.org.uk', [volunteer.email]
-        )
-        logo_path = '/code/static-built/img/tofro-logo-knockout.png'
-        logo_cid = attach_inline_image_file(message, logo_path)
-        context['logo_cid'] = logo_cid
-
-        kites_path = '/code/static-built/img/tofro-kites.png'
-        kites_cid = attach_inline_image_file(message, kites_path)
-        context['kites_cid'] = kites_cid
-
-        html_body = render_to_string(template_file, context)
-        message.attach_alternative(html_body, 'text/html')
-
-        message.send()
-
-
-        return
-
-        images = [  # svg+xml
-            ('/code/static-built/img/tofro-kites.png', 'png', 'tofro-kites'),
-            ('/code/static-built/img/tofro-logo-knockout.png', 'png', 'tofro-logo-knockout')
-        ]
-        attachments = []
-        for filepath, subtype, content_id in images:
-            with open(filepath, 'rb') as f:
-                msgImage = MIMEImage(f.read(), _subtype=subtype)
-                msgImage.add_header('Content-ID', content_id)
-                msgImage.add_header('X-Attachment-Id', content_id)
-                msgImage.add_header("Content-Disposition", "inline", filename=content_id)
-                attachments.append(msgImage)
-
-        email_msg = EmailMessage(
-            subject_title, html_body,
-            bcc=[volunteer.email], attachments=attachments
-        )
-        email_msg.content_subtype = "html"
-        email_msg.send()
-
-        #send_email(
-        #    subject_title, None, [volunteer.email], html_message=html_body
-        #)
+        print(f"sending email to Volunteer {volunteer.pk}: {volunteer.email}")
+        try:
+            email_msg.send(fail_silently=False)
+        except SMTPException as e:
+            logger.error(f"SMTPException thrown sending email for volunteer {volunteer.pk}: {e}")
