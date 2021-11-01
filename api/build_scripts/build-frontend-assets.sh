186b58a983a936876b12a9dc45badd765a994dbc
Dan Tagg <dantagg@wildmanherring.com>
2021-11-01 16:31:52 +0000

    Fixed bash bugs (line continuation) in shell script

--- api/build_scripts/build-frontend-assets.sh
+++ api/build_scripts/build-frontend-assets.sh
@@ -7,11 +7,11 @@ TOFRO_API_BASE_DIR=/code
 
 if [[ $RUN_ENV == "local-dev" || $RUN_ENV == "dev" ]]; then
   NODE_ENV=development
-    parcel build "$TOFRO_API_BASE_DIR/static-src/*" -d "$TOFRO_API_BASE_DIR/parcel-built" --public-url
+    parcel build "$TOFRO_API_BASE_DIR/static-src/*" -d "$TOFRO_API_BASE_DIR/parcel-built" --public-url \
       '.' --no-minify
 else
   NODE_ENV=production
-    parcel build "$TOFRO_API_BASE_DIR/static-src/*" -d "$TOFRO_API_BASE_DIR/parcel-built" --public-url
+    parcel build "$TOFRO_API_BASE_DIR/static-src/*" -d "$TOFRO_API_BASE_DIR/parcel-built" --public-url \
       '.'
 fi
 
