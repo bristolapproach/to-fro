81548bb904e32c8b40502de48eed00d0a37aaa01
Dan Tagg <dantagg@wildmanherring.com>
2021-12-19 15:31:49 +0000

    Use template tags rather than hard coded

--- api/notifications/utils.py
+++ api/notifications/utils.py
@@ -8,8 +8,9 @@ def gen_subject_and_message(site_url, notification_type, action=None, context={}
     also be provided as context to the message template.
     """
     # Generate URLs for the templates.
+    admin_action_url = reverse('admin:actions_action_change', args=[action.id])
     context['action_url'] = f'{site_url}{action.get_absolute_url()}'
-    context['admin_action_url'] = f'{site_url}/admin/actions/action/{action.id}/change'
+    context['admin_action_url'] = f'{site_url}{admin_action_url}'
 
     # Return the subject and message, removing line breaks from the subject.
     return (render_to_string(f'notifications/{notification_type.lower()}_subject.txt', context).replace('\n', ' ').replace('\r', ' '),
