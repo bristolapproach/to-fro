5947ff0269e5ff2241c0d810f44fbe804fcce32a
Dan Tagg <dantagg@wildmanherring.com>
2021-12-19 15:30:10 +0000

    run set_password to try and prevent enumeration attacks

--- api/core/backends.py
+++ api/core/backends.py
@@ -17,8 +17,9 @@ class EmailBackend(ModelBackend):
             user = UserModel.objects.get(
                 Q(email__iexact=username) | Q(username=username))
         except UserModel.DoesNotExist:
-            return None
+            UserModel().set_password(password)
+            pass
         else:
             if user.check_password(password):
                 return user
-        return None
+        #return None
